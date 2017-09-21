24aa959d8e86092660706a4748f3d6f7453f4c83
모바일개발팀 <daegon@mobailgaebaltim-ui-iMac.local>
2017-09-21 15:11:45 +0900

    초기 소스코드

---
+++ f4ium-ios/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  f4ium-ios
+//
+//  Created by 모바일개발팀 on 2017. 9. 21..
+//  Copyright © 2017년 모바일개발팀. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ f4ium-ios/Other Sources/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  f4ium-ios
+//
+//  Created by 모바일개발팀 on 2017. 9. 21..
+//  Copyright © 2017년 모바일개발팀. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
